68db0b99a4f9ed8aa000e0e7e7c2ae40f9096edd
Adam Zielonka <root@adamzielonka.pro>
2019-03-12 20:28:32 +0100

    moved from static to react page

--- src/App.js
+++ src/App.js
@@ -1,28 +1,57 @@
-import React, { Component } from 'react'
-import logo from './logo.svg'
+import React from 'react'
 import './App.css'
+import { lines } from './store'
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    )
+function User() {
+  return <>
+    <span className="user">root@adamzielonka.pro</span>
+    <span className="user-end">:~# </span>
+  </>
+}
+
+function Line({ user, line }) {
+  return <li>
+    {user ? <User /> : ''}
+    {line ? <LineWithUrls line={line} />: <>&nbsp;</> }
+  </li>
+}
+
+
+function slice(line) {
+  let m = line.match(/\[(.*?)\]\((.*?)\)/)
+
+  if(m) {
+    const i = line.indexOf(m[0])
+
+    return [
+      { text: line.slice(0, i) },
+      { text: m[1], url: m[2] },
+      ...slice(line.slice(m[0].length + i, line.length))
+    ]
   }
+
+  return [{ text: line }]
+}
+
+function LineWithUrls({ line }) {
+  let count = 0
+  return <>
+    {slice(line).map(l => <span key={count++} >
+      {l.url ? <a href={l.url}>{l.text}</a> : l.text}
+    </span>)}
+  </>
+}
+
+function App() {
+  let count = 0
+
+  return (
+    <div className="App">
+      <ul>
+        {lines.map(line => <Line key={count++} {...line} />)}
+      </ul>
+    </div>
+  )
 }
 
 export default App
