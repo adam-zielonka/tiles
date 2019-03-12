e6c4a882a7b0a57f8000fbbfe57029d5d58aa894
Adam Zielonka <root@adamzielonka.pro>
2019-03-12 21:57:41 +0100

    auto typin command

--- src/App.js
+++ src/App.js
@@ -42,15 +42,36 @@ function LineWithUrls({ line }) {
 }
 
 const iterator = getLine()
+let str = ''
+let done = true
 
 function App() {
   const [list, setList] = useState([])
 
   useEffect(() => {
-    const line = iterator.next()
-    if(!line.done) setTimeout(() => {
-      setList([...list, line.value])
-    }, line.value.time)
+    if(done) {
+      const line = iterator.next()
+      if(!line.done) setTimeout(() => {
+        if(line.value.user) {
+          done = false
+          str = line.value.line
+          line.value.line = ''
+        }
+        setList([...list, line.value])
+      }, line.value.time)
+    } else {
+      const line = list.pop()
+      if(line) {
+        const newLine = line.line + str.slice(line.line.length, line.line.length + 1)
+        if(newLine !== line.line) {
+          line.line = newLine
+          setTimeout(() => setList([...list, line]), 50)
+        } else {
+          done = true
+          setTimeout(() => setList([...list, line]))
+        }
+      }
+    }
   })
 
   let count = 0
