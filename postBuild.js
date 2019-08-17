e461935b0333fb704013e1ecd6852d4d05298476
Adam Zielonka <root@adamzielonka.pro>
2019-08-17 11:26:52 +0200

    Remove command: version

--- postBuild.js
+++ postBuild.js
@@ -1,5 +1,4 @@
 import links from './src/links.json'
-import packageJson from './package.json'
 import replace from 'replace-in-file'
 import { slice } from './src/utils.js'
 
@@ -12,22 +11,6 @@ replace({
   files: './build/index.html',
   from: /<links\/>/,
   to: html,
-}).then(results => {
-  if(results[0].hasChanged)
-    console.log('Added links to no-script section')
-  else
-    console.error('Not found <links/> tag')
-}).catch(error => {
-  console.error('Error occurred:', error)
-})
-
-replace({
-  files: './build/index.html',
-  from: /<version\/>/,
-  to: `<script>
-    const VERSION = '${packageJson.version}';
-    const BUILD_TIME = '${Date()}';
-  </script>`,
 }).then(results => {
   if(results[0].hasChanged)
     console.log('Added links to no-script section')
