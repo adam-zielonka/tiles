668a3ce8a51102030db6c6b1399c1865cb659b62
Adam Zielonka <root@adamzielonka.pro>
2019-10-13 11:54:38 +0200

    Unnessery, so I deleted

--- src/components/Line.jsx
+++ src/components/Line.jsx
@@ -3,7 +3,7 @@ import { parseText } from '../utils/line'
 
 export function TextLine({ children }) {
   return <li>
-    {parseText(children ? children : '\u00a0').map((l, i) => <span key={i}>
+    {parseText(children || '\u00a0').map((l, i) => <span key={i}>
       {l.url ? <a href={l.url}>{l.text}</a> : l.text}
     </span>)}
   </li>
