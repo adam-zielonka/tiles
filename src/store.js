646356d345cce43812dde9ebfabc054fa0652f64
Adam Zielonka <root@adamzielonka.pro>
2020-09-05 20:15:27 +0200

    No start commands in dev mode

--- src/store.js
+++ src/store.js
@@ -11,7 +11,7 @@ class Store {
     this.lines = []
     this.toProcess = []
     this.isProcessing = false
-    this.startCommand = ['whoami', 'description']
+    this.startCommand = process.env.NODE_ENV !== 'production' ? [] : ['whoami', 'description']
     this.history = []
     this.lastCommand = ''
     this.historyPosition = this.history.length
