6a8cce82c1f45bbd3937b1cc3ee1025c26c14b01
Adam Zielonka <root@adamzielonka.pro>
2020-09-05 22:39:52 +0200

    I don't plan to use service worker in this project

--- src/index.jsx
+++ src/index.jsx
@@ -1,9 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import './index.styl'
-import * as serviceWorker from './serviceWorker'
 import Terminal from './Terminal'
 
 ReactDOM.render(<Terminal />, document.getElementById('root'))
-
-serviceWorker.unregister()
