af086dc4de1edd25086b038f1db747ece4699aef
Adam Zielonka <root@adamzielonka.pro>
2020-09-06 12:27:10 +0200

    When system is on

--- src/Terminal.test.jsx
+++ src/Terminal.test.jsx
@@ -1,10 +1,18 @@
 import React from 'react'
 import { shallow } from 'enzyme'
 import Terminal from './Terminal'
+import { Shutdown } from './components/Shutdown'
+
 describe('<Terminal/>', () => {
   it('System is off', () => {
     window.store.shutdown = true
     const wrapper = shallow(<Terminal/>)
-    expect(wrapper.text()).toEqual('<Shutdown />')
+    expect(wrapper.containsMatchingElement(<Shutdown/>)).toEqual(true)
+  })
+
+  it('System is on', () => {
+    window.store.shutdown = false
+    const wrapper = shallow(<Terminal/>)
+    expect(wrapper.text()).toEqual('')
   })
 })
