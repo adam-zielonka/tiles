4bb9ebb7e5b5a7e0e1cae297633861e38e0d707d
Adam Zielonka <root@adamzielonka.pro>
2020-10-22 21:33:33 +0200

    System has lines

--- src/Terminal.test.jsx
+++ src/Terminal.test.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import { shallow } from 'enzyme'
 import Terminal from './Terminal'
 import { Shutdown } from './components/Shutdown'
+import { Line } from './components/Line'
 
 describe('<Terminal/>', () => {
   it('System is off', () => {
@@ -14,5 +15,13 @@ describe('<Terminal/>', () => {
     window.store.shutdown = false
     const wrapper = shallow(<Terminal/>)
     expect(wrapper.text()).toEqual('')
+  })
+
+  it('System has lines', () => {
+    window.store.lines.push({ command: 'echo test' })
+    window.store.lines.push({ text: 'test' })
+    const wrapper = shallow(<Terminal/>)
+    expect(wrapper.containsMatchingElement(<Line/>)).toEqual(true)
+    expect(wrapper.text()).toEqual('<Line /><Line />')
   })
 })
