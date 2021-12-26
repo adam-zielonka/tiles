c28be18a90350877b432fe5e203ed06aa83bf1b3
Adam Zielonka <root@adamzielonka.pro>
2021-12-26 10:09:25 +0100

    More cd functions

--- src/store/path.ts
+++ src/store/path.ts
@@ -4,9 +4,27 @@ export const path = writable('~')
 
 export const cd = (newDir: string) => {
   path.update(dir => {
-    if (newDir.match(/^~/)) return newDir
-    if (newDir.match(/^\//)) return newDir
-
-    return dir + '/' + newDir
+    if (newDir === '..') {
+      const parts = dir.split('/')
+      parts.pop()
+      if (parts.length === 0 || parts[0] === '') {
+        return '/'
+      }
+      return parts.join('/')
+    } else if (newDir === '.') {
+      return dir
+    } else if (newDir === '~') {
+      return '~'
+    } else if (newDir.startsWith('/')) {
+      return newDir
+        .replace(/\/\//, '/')
+        .replace(/.\/$/, '')
+        .replace(/^\/root/, '~')
+    } else {
+      return `${dir}/${newDir}`
+        .replace(/\/\//, '/')
+        .replace(/.\/$/, '')
+        .replace(/^\/root/, '~')
+    }
   })
 }
