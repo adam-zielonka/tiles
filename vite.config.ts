29f92a908c507875667d881091b06afce7b8ae0d
Adam Zielonka <root@adamzielonka.pro>
2023-02-05 22:32:08 +0100

    Type vite config

--- vite.config.ts
+++ vite.config.ts
@@ -1,16 +1,17 @@
-import { defineConfig } from "vite";
+import { defineConfig, PluginOption } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import frontMatter from "front-matter";
 
-const markdownProcessor = {
+const markdownProcessor: PluginOption = {
   name: "markdown-processor",
   transform: (code, id) => {
     if (!id.endsWith(".md")) return null;
 
-    const { attributes, body } = frontMatter(code);
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    const { attributes, body } = frontMatter<any>(code);
 
     return `export default ${JSON.stringify({
-      ...attributes as any,
+      ...attributes,
       body,
     })}`;
   },
