86593acaa16bebf496fb4fe9b13771c43d30070b
Adam Zielonka <root@adamzielonka.pro>
2023-02-07 07:54:22 +0100

    Update Completion.tsx

--- src/components/Completion.tsx
+++ src/components/Completion.tsx
@@ -1,22 +1,12 @@
-import { observer } from "mobx-react-lite";
 import { useEffect } from "react";
-import { store } from "../store/store";
 import "./Completion.scss";
 
-export const Completion = observer(() => {
-  const { list, index, isVisible } = store.completion;
-  
-  useEffect(() => {
-    isVisible && window.scrollTo(0, document.body.scrollHeight)
-  }, [isVisible]);
-
-  if (!isVisible) {
-    return null;
-  }
+export function Completion({ list, index }:{ list: string[], index: number}) {
+  useEffect(() => window.scrollTo(0, document.body.scrollHeight));
 
   return <li className="Completion">
     {list.map((completion, i) => <div key={i} className={i === index ? "active" : ""}>
       {completion}
     </div>)}
   </li>;
-});
+}
