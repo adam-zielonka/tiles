68d361b6af4ccc7e3332617a992e54090c1789c8
Adam Zielonka <root@adamzielonka.pro>
2023-04-09 11:30:03 +0200

    Split output to more files

--- vite.config.ts
+++ vite.config.ts
@@ -26,5 +26,18 @@ export default defineConfig({
   plugins: [react(), markdownProcessor],
   build: {
     sourcemap: true,
+    rollupOptions: {
+      output: {
+        manualChunks: (id) => {
+          if (id.includes("node_modules")) {
+            return "vendor";
+          }
+        
+          if (id.includes(".md")) {
+            return "commands";
+          }
+        }
+      },
+    },
   }
 });
