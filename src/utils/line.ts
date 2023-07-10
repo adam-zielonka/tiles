bbd155a693ac688339bdf67df6ed5a0a65cf5556
Adam Zielonka <root@adamzielonka.pro>
2023-07-10 21:47:05 +0200

    Add stylistic eslint for ts

--- src/utils/line.ts
+++ src/utils/line.ts
@@ -1,5 +1,5 @@
 import { CommandLine, OutputLine } from "../types/line";
 
 export function isCommandLine(line: OutputLine): line is CommandLine {
-  return (<CommandLine>line).path !== undefined;
+  return (line as CommandLine).path !== undefined;
 }
