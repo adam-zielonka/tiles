4f3c27e3ff4d62d1b36661371885483dae9aea3e
Adam Zielonka <root@adamzielonka.pro>
2023-07-26 21:45:16 +0200

    Remove no script url param

--- src/main.tsx
+++ src/main.tsx
@@ -1,17 +1,13 @@
+document.querySelector("#noscript")?.remove();
+
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { Terminal } from "./components/terminal";
 
-const urlParams = new URLSearchParams(window.location.search);
+import.meta.glob("./styles/*.scss", { eager: true });
 
-if (!urlParams.has("noscript")) {
-  document.querySelector("#noscript")?.remove();
-
-  import.meta.glob("./styles/*.scss", { eager: true });
-
-  ReactDOM.createRoot(document.getElementById("root")!).render(
-    <React.StrictMode>
-      <Terminal />
-    </React.StrictMode>,
-  );
-}
+ReactDOM.createRoot(document.getElementById("root")!).render(
+  <React.StrictMode>
+    <Terminal />
+  </React.StrictMode>,
+);
