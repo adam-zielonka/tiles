5d29ce63b92828c97b29cd17c12ae0adac1a5017
Adam Zielonka <root@adamzielonka.pro>
2023-08-13 00:07:11 +0200

    Background clip for progress

--- src/tiles/Tiles.tsx
+++ src/tiles/Tiles.tsx
@@ -33,15 +33,17 @@ type TileProps = {
 }
 
 function Tile({ title, description, value, info, leftInfo }: TileProps) {
-  return <div className="Tile">
-    <header>
-      <h1>{title}</h1>
-      <h2>{description}</h2>
-    </header>
-    <main>{value}</main>
-    <footer>
-      <p>{leftInfo}</p>
-      <p title={info}>{info}</p>
-    </footer>
+  return <div className="Wrapper">
+    <div className="Tile">
+      <header>
+        <h1>{title}</h1>
+        <h2>{description}</h2>
+      </header>
+      <main>{value}</main>
+      <footer>
+        <p>{leftInfo}</p>
+        <p title={info}>{info}</p>
+      </footer>
+    </div>
   </div>
 }
