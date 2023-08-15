c0dcb88d6fe23cc5dd556e1c2af0a7b18a81288b
Adam Zielonka <root@adamzielonka.pro>
2023-08-15 22:21:26 +0200

    Remove wrong commited files

--- src/tiles/Highlight.tsx
+++
@@ -1,8 +0,0 @@
-import { observer } from "mobx-react-lite";
-import { store } from "./store";
-
-export const Highlight = observer(function Highlight({ children }: { children: string }) {
-  if (!store.highlight) return <span>{children}</span>;
-
-  return <span className="Highlight">{children}</span>;
-});
