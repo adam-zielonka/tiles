a23f239ed72de6237324637c7f47369170bd5ab1
Adam Zielonka <root@adamzielonka.pro>
2024-05-06 22:27:05 +0200

    No need to calculate progress size with js

--- src/components/tile.tsx
+++ src/components/tile.tsx
@@ -1,10 +1,8 @@
-import { useLayoutEffect, useRef, useState } from "react";
 import { COLORS, Intent } from "../store/colors";
 import { Highlight } from "./highlight";
-import { store } from "../store/store";
-import { observer } from "mobx-react-lite";
 import { CubeIcon, BoxMultipleIcon, CubeOutlineIcon, BoxMultipleOutlineIcon } from "./icons";
 
+type TileIcon = "plan" | "transfer" | "pick" | "post";
 export type TileProps = {
   title: string;
   description?: string;
@@ -13,58 +11,42 @@ export type TileProps = {
   leftInfo?: string;
   intent?: Intent;
   progress?: number;
-  icon: "plan" | "transfer" | "pick" | "post";
+  icon: TileIcon;
 }
 
-export const Tile = observer(function ({ title, description, value, info, leftInfo, intent = "default", progress = 0,
-  icon }: TileProps) {
+export function Tile({ 
+  title, description, value, info, leftInfo, intent = "default", progress = 0, icon
+}: TileProps) {
 
-  const ref = useRef<HTMLDivElement>(null);
-  const [width, setWidth] = useState(240);
-
-  useLayoutEffect(() => {
-    if (ref.current) {
-      setWidth(ref.current.clientWidth);
-    }
-  }, [store.dimensions.width]);
-
-
-  function Icon() {
-    switch (icon) {
-      case "plan": return <BoxMultipleIcon/>;
-      case "transfer": return <CubeIcon/>;
-      case "pick": return <CubeOutlineIcon/>;
-      case "post": return <BoxMultipleOutlineIcon/>;
-    }
-  }
-
-  const insideTile = <div className="Tile">
-    <section>
-      <header>
-        <h1><Highlight>{title}</Highlight></h1>
-        <h2>{description}</h2>
-      </header>
-      <main>
-        <Icon/>
-        {value}
-      </main>
-    </section>
-    <section>
-      <p>{leftInfo}</p>
-      <p title={info}>{info}</p>
-    </section>
-  </div>;
-
-  return <div ref={ref} style={{
+  return <div style={{
     "--tile-color": COLORS[intent].background,
     "--tile-font-color": COLORS[intent].font ?? "black",
-    "--tile-highlight-color": COLORS[intent].font ? COLORS[intent].background : "black",
+    "--tile-progress": `${intent !== "default" ? 0 : progress}%`,
   }}>
-    {insideTile}
-    {progress > 0 && <div className="tile-progress" style={{ width: `${progress}%` }}>
-      <div style={{ width }}>
-        {insideTile}
-      </div>
-    </div>}
+    <div className="Tile">
+      <section>
+        <header>
+          <h1><Highlight>{title}</Highlight></h1>
+          <h2>{description}</h2>
+        </header>
+        <main>
+          <TileIcon icon={icon}/>
+          {value}
+        </main>
+      </section>
+      <section>
+        <p>{leftInfo}</p>
+        <p title={info}>{info}</p>
+      </section>
+    </div>
   </div>;
-});
+}
+
+function TileIcon({ icon }:{ icon: TileIcon }) {
+  switch (icon) {
+    case "plan": return <BoxMultipleIcon/>;
+    case "transfer": return <CubeIcon/>;
+    case "pick": return <CubeOutlineIcon/>;
+    case "post": return <BoxMultipleOutlineIcon/>;
+  }
+}
